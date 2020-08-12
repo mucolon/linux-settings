516c7412a0d7e59c84e3ce77675b97a96a5132d5
Marco U. Colón <marcocolon@live.com>
2020-08-12 02:43:28 -0700

    updated bashrc-append.sh

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -1,34 +1,44 @@
 # store colors
-MAGENTA="\[\033[0;35m\]"
-YELLOW="\[\033[01;33m\]"
+MAGENTA="\[\033[00;35m\]"
+MAGENTA_BOLD="\[\033[01;35m\]"
+YELLOW="\[\033[00;33m\]"
+YELLOW_BOLD="\[\033[01;33m\]"
 BLUE="\[\033[00;34m\]"
-LIGHT_GRAY="\[\033[0;37m\]"
-CYAN="\[\033[0;36m\]"
+BLUE_BOLD="\[\033[01;34m\]"
+LIGHT_GRAY="\[\033[00;37m\]"
+LIGHT_GRAY_BOLD="\[\033[01;37m\]"
+CYAN="\[\033[00;36m\]"
+CYAN_BOLD="\[\033[01;36m\]"
 GREEN="\[\033[00;32m\]"
-RED="\[\033[0;31m\]"
-VIOLET='\[\033[01;35m\]'
+GREEN_BOLD="\[\033[01;32m\]"
+RED="\[\033[00;31m\]"
+RED_BOLD="\[\033[01;31m\]"
+VIOLET="\[\033[00;35m\]"
+VIOLET_BOLD="\[\033[01;35m\]"
+DEFAULT="\[\033[00m\]"
 
 function color_my_prompt {
-  local __user_and_host="$GREEN\u@\h"
-  local __cur_location="$BLUE\W"           # capital 'W': current directory, small 'w': full file path
-  local __git_branch_color="$GREEN"
-  local __prompt_tail="$VIOLET$"
-  local __user_input_color="$GREEN"
+  local __user_and_host="$GREEN_BOLD\u@\h"
+  local __colon="$DEFAULT:"
+  local __cur_location="$BLUE_BOLD\w"           # capital 'W': current directory, small 'w': full file path
+  local __git_branch_color="$GREEN_BOLD"
+  local __prompt_tail="$VIOLET_BOLD\n$"
+  local __user_input_color="$DEFAULT"
   local __git_branch=$(__git_ps1);
 
   # colour branch name depending on state
   if [[ "${__git_branch}" =~ "*" ]]; then     # if repository is dirty
-      __git_branch_color="$RED"
+      __git_branch_color="$RED_BOLD"
   elif [[ "${__git_branch}" =~ "$" ]]; then   # if there is something stashed
-      __git_branch_color="$YELLOW"
+      __git_branch_color="$YELLOW_BOLD"
   elif [[ "${__git_branch}" =~ "%" ]]; then   # if there are only untracked files
-      __git_branch_color="$LIGHT_GRAY"
+      __git_branch_color="$LIGHT_GRAY_BOLD"
   elif [[ "${__git_branch}" =~ "+" ]]; then   # if there are staged files
-      __git_branch_color="$CYAN"
+      __git_branch_color="$CYAN_BOLD"
   fi
 
   # Build the PS1 (Prompt String)
-  PS1="$__user_and_host $__cur_location$__git_branch_color$__git_branch $__prompt_tail$__user_input_color "
+  PS1="$__user_and_host$__colon$__cur_location$__git_branch_color$__git_branch $__prompt_tail$__user_input_color "
 }
 
 # configure PROMPT_COMMAND which is executed each time before PS1
