5632fe116f15c8858da7b104b9afc998121696c2
Marco U. Colón <marcocolon@live.com>
2020-08-11 23:23:43 -0700

    added settup script

---
+++ setup-settings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "Starting script"
+cd ~
+cp ~/linux-settings/.git-prompt.sh ~/git-prompt.sh
+
+cat ~/linux-settings/bashrc-append.sh >> ~/.bashrc
+
+bash -l
+
+echo
+echo "Done"
